1b177d9e4894ac792be3b8b86be79993c728ac33
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-07-25 18:37:10 -0500

    Change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Logo from '../img/logo-black.png'
 import '../styles/navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src = 'https://iconape.com/wp-content/png_logo_vector/flutter-logo.png' />
+        <img alt = 'logo' src = 'https://iconape.com/wp-content/png_logo_vector/flutter-logo.png' />
     </div>
   )
 }
